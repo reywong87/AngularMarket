d3a0f6c802adb5737cfc4f2a3525f891775012b3
Rey <interactivaomega@gmail.com>
2025-04-03 13:14:51 +0200

    Load post images from https://picsum.photos/

--- src/blog/blog-item/blog-item.component.ts
+++ src/blog/blog-item/blog-item.component.ts
@@ -1,18 +1,26 @@
-import {Component, input} from '@angular/core';
+import {Component, input, OnInit, signal} from '@angular/core';
 import {IPost} from "../../interfaces/i-post";
-import {NgStyle} from "@angular/common";
 import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-blog-item',
     imports: [
-        NgStyle,
         RouterLink
     ],
     templateUrl: './blog-item.component.html',
     styleUrl: './blog-item.component.css'
 })
-export class BlogItemComponent {
-  post = input.required<IPost>();
-
+export class BlogItemComponent implements OnInit {
+    public post = input.required<IPost>();
+    public imageUrl = signal("");
+        
+    ngOnInit(): void {
+        this.imageUrl.set(`https://picsum.photos/id/${this.getRandomNumber()}/400`)
+    }
+    
+    getRandomNumber(): string{
+        let id = this.post().id
+        let result = 155 + id
+        return result.toString();
+    }
 }
